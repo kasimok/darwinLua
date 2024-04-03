083b3eb9f5a2318b952d9950c54e940af23913c1
0x67 <evilisnj@gmail.com>
2024-04-03 16:21:56 +0800

    done the poc.

--- lua/ViewController.swift
+++ lua/ViewController.swift
@@ -10,8 +10,27 @@ import liblua
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var outputLabel: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let l = luaL_newstate()
+    luaL_openlibs(l)
+    
+    let script = "return 'Hello World from lua!'"
+    luaL_loadstring(l, script)
+    
+    lua_pcallk(l, 0, LUA_MULTRET, 0, 0, nil)
+    
+    // Close the Lua state
+    
+    var len: size_t = 0 // Variable to hold the string length
+    if let cString = luaL_tolstring(l, -1, &len) {
+      let swiftString = String(cString: cString)
+      outputLabel.text = swiftString
+    }
+    
+    lua_close(l)
   }
 
 
